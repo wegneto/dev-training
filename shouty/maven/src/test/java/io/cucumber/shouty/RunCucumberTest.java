3e9f4b95735ad79637136b3a113898b186b944f9
Wilson Guimarães <wegneto@gmail.com>
2023-06-08 12:35:03 +0100

    BDD with Cucumber: More Control

--- shouty/maven/src/test/java/io/cucumber/shouty/RunCucumberTest.java
+++ shouty/maven/src/test/java/io/cucumber/shouty/RunCucumberTest.java
@@ -5,8 +5,8 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
