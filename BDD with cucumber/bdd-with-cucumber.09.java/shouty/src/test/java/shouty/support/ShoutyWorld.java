1b5411e81e32321e2e9121811344f7f9c964b290
Wilson Guimarães <wegneto@gmail.com>
2023-11-18 11:44:59 +0000

    BDD with cucumber: Chapter 09 - Dependency Injection

---
+++ BDD with cucumber/bdd-with-cucumber.09.java/shouty/src/test/java/shouty/support/ShoutyWorld.java
@@ -0,0 +1,8 @@
+package shouty.support;
+
+import shouty.Network;
+
+public class ShoutyWorld {
+    public static final int DEFAULT_RANGE = 100;
+    public Network network = new Network(ShoutyWorld.DEFAULT_RANGE);
+}
