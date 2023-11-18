4e058742c9fc764781c2dd44f921d2578d6a412e
Wilson Guimarães <wegneto@gmail.com>
2023-11-18 12:22:47 +0000

    BDD with cucumber: Chapter 09 - Extracting Support Code

--- BDD with cucumber/bdd-with-cucumber.09.java/shouty/src/test/java/shouty/StepDefinitions.java
+++ BDD with cucumber/bdd-with-cucumber.09.java/shouty/src/test/java/shouty/StepDefinitions.java
@@ -2,12 +2,10 @@ package shouty;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import io.cucumber.datatable.DataTable;
-import io.cucumber.java.Before;
 import io.cucumber.java.DataTableType;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -21,8 +19,6 @@ import static org.junit.Assert.assertEquals;
 public class StepDefinitions {
 
     private final ShoutyWorld world;
-    private Map<String, Person> people;
-    private Map<String, List<String>> messagesShoutedBy;
 
     public StepDefinitions(ShoutyWorld world) {
         this.world = world;
@@ -33,111 +29,89 @@ public class StepDefinitions {
         return new Whereabouts(entry.get("name"), Integer.parseInt(entry.get("location")));
     }
 
-    @Before
-    public void setup() {
-        people = new HashMap<String, Person>();
-        messagesShoutedBy = new HashMap<String, List<String>>();
-    }
-
     @Given("the range is {int}")
     public void the_range_is(int range) throws Throwable {
         world.network = new Network(range);
     }
 
-    @Given("{word} is located at {int}")
-    public void person_is_located_at(String name, Integer location) {
-        Person person = new Person(name, world.network, location);
-        people.put(person.getName(), person);
+    @Given("{person} is located at {int}")
+    public void person_is_located_at(Person person, Integer location) {
+        person.moveTo(location);
     }
 
-    @Given("{word} has bought {int} credits")
-    public void person_has_bought_credits(String name, int credits) {
-        people.get(name).setCredits(credits);
+    @Given("{person} has bought {int} credits")
+    public void person_has_bought_credits(Person person, int credits) {
+        person.setCredits(credits);
     }
 
-    @When("{word} shouts")
-    public void person_shouts(String name) throws Throwable {
-        Person person = people.get(name);
-        shout(person, "Hello, world");
+    @When("{person} shouts")
+    public void person_shouts(Person person) throws Throwable {
+        world.shout(person, "Hello, world", this);
     }
 
-    @When("{word} shouts {string}")
-    public void person_shouts_message(String name, String message) throws Throwable {
-        Person person = people.get(name);
-        shout(person, message);
+    @When("{person} shouts {string}")
+    public void person_shouts_message(Person person, String message) throws Throwable {
+        world.shout(person, message, this);
     }
 
-    @When("{word} shouts {int} messages containing the word {string}")
-    public void person_shouts_messages_containing_the_word(String name, int count, String word) throws Throwable {
-        Person person = people.get(name);
+    @When("{person} shouts {int} messages containing the word {string}")
+    public void person_shouts_messages_containing_the_word(Person person, int count, String word) throws Throwable {
         String message = "a message containing the word " + word;
         for (int i = 0; i < count; i++) {
-            shout(person, message);
+            world.shout(person, message, this);
         }
     }
 
-    @When("{word} shouts the following message")
-    public void person_shouts_the_following_message(String name, String message) throws Throwable {
-        Person person = people.get(name);
-        shout(person, message);
+    @When("{person} shouts the following message")
+    public void person_shouts_the_following_message(Person person, String message) throws Throwable {
+        world.shout(person, message, this);
     }
 
-    @When("{word} shouts {int} over-long messages")
-    public void person_shouts_some_over_long_messages(String name, int count) throws Throwable {
-        Person person = people.get(name);
+    @When("{person} shouts {int} over-long messages")
+    public void person_shouts_some_over_long_messages(Person person, int count) throws Throwable {
         String baseMessage = "A message from Sean that is 181 characters long ";
         String padding = "x";
         String overlongMessage = baseMessage + padding.repeat(181 - baseMessage.length());
 
         for (int i = 0; i < count; i++) {
-            shout(person, overlongMessage);
+            world.shout(person, overlongMessage, this);
         }
     }
 
-    @Then("{word} should hear Sean's message")
-    public void person_hears_Sean_s_message(String name) throws Throwable {
-        List<String> messages = messagesShoutedBy.get("Sean");
-        assertEquals(messages, people.get(name).getMessagesHeard());
+    @Then("{person} should hear Sean's message")
+    public void person_hears_Sean_s_message(Person person) throws Throwable {
+        List<String> messages = world.messagesShoutedBy.get("Sean");
+        assertEquals(messages, person.getMessagesHeard());
     }
 
-    @Then("{word} should not hear a shout")
-    public void person_should_not_hear_a_shout(String name) throws Throwable {
-        assertEquals(0, people.get(name).getMessagesHeard().size());
+    @Then("{person} should not hear a shout")
+    public void person_should_not_hear_a_shout(Person person) throws Throwable {
+        assertEquals(0, person.getMessagesHeard().size());
     }
 
-    @Then("{word} hears the following messages:")
-    public void person_hears_the_following_messages(String name, DataTable expectedMessages) {
-        List<List<String>> actualMessages = new ArrayList<List<String>>();
-        List<String> heard = people.get(name).getMessagesHeard();
+    @Then("{person} hears the following messages:")
+    public void person_hears_the_following_messages(Person person, DataTable expectedMessages) {
+        List<List<String>> actualMessages = new ArrayList<>();
+        List<String> heard = person.getMessagesHeard();
         for (String message : heard) {
             actualMessages.add(Collections.singletonList(message));
         }
         expectedMessages.diff(DataTable.create(actualMessages));
     }
 
-    @Then("{word} hears all Sean's messages")
-    public void person_hears_all_Sean_s_messages(String name) throws Throwable {
-        List<String> heardByLucy = people.get(name).getMessagesHeard();
-        List<String> messagesFromSean = messagesShoutedBy.get("Sean");
+    @Then("{person} hears all Sean's messages")
+    public void person_hears_all_Sean_s_messages(Person person) throws Throwable {
+        List<String> heardByLucy = person.getMessagesHeard();
+        List<String> messagesFromSean = world.messagesShoutedBy.get("Sean");
 
         // Hamcrest's hasItems matcher wants an Array, not a List.
         String[] messagesFromSeanArray = messagesFromSean.toArray(new String[messagesFromSean.size()]);
         assertThat(heardByLucy, hasItems(messagesFromSeanArray));
     }
 
-    @Then("{word} should have {int} credits")
-    public void person_should_have_credits(String name, int expectedCredits) {
-        assertEquals(expectedCredits, people.get(name).getCredits());
-    }
-
-    private void shout(Person person, String message) {
-        person.shout(message);
-        List<String> messages = messagesShoutedBy.get(person.getName());
-        if (messages == null) {
-            messages = new ArrayList<String>();
-            messagesShoutedBy.put(person.getName(), messages);
-        }
-        messages.add(message);
+    @Then("{person} should have {int} credits")
+    public void person_should_have_credits(Person person, int expectedCredits) {
+        assertEquals(expectedCredits, person.getCredits());
     }
 
     static class Whereabouts {
--- BDD with cucumber/bdd-with-cucumber.09.java/shouty/src/test/java/shouty/support/ShoutyWorld.java
+++ BDD with cucumber/bdd-with-cucumber.09.java/shouty/src/test/java/shouty/support/ShoutyWorld.java
@@ -1,8 +1,28 @@
 package shouty.support;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import shouty.Network;
+import shouty.Person;
+import shouty.StepDefinitions;
 
 public class ShoutyWorld {
+
     public static final int DEFAULT_RANGE = 100;
     public Network network = new Network(ShoutyWorld.DEFAULT_RANGE);
+    public Map<String, Person> people = new HashMap<>();
+    public Map<String, List<String>> messagesShoutedBy = new HashMap<>();
+
+    public void shout(Person person, String message, StepDefinitions stepDefinitions) {
+        person.shout(message);
+        List<String> messages = messagesShoutedBy.get(person.getName());
+        if (messages == null) {
+            messages = new ArrayList<>();
+            messagesShoutedBy.put(person.getName(), messages);
+        }
+        messages.add(message);
+    }
 }
